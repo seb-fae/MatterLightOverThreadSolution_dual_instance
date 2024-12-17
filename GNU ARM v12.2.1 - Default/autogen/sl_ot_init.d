eab975396a3e00d76637b6fa2c490128f31ca8d6
secerdan <secerdan@silabs.com>
2024-12-17 11:51:37 +0100

    Remove GNU folder

--- GNU ARM v12.2.1 - Default/autogen/sl_ot_init.d
+++
@@ -1,9 +0,0 @@
-$@: ../autogen/sl_ot_init.c \
- C:/Users/secerdan/SimplicityStudio/v5_workspace_12/MatterLightOverThreadSolution_dual_instance/config/sl_openthread_ble_cli_config.h \
- C:/Users/secerdan/SimplicityStudio/v5_workspace_12/MatterLightOverThreadSolution_dual_instance/config/sl_openthread_rtos_config.h \
- C:/Users/secerdan/SimplicityStudio/v5_workspace_12/MatterLightOverThreadSolution_dual_instance/simplicity_sdk_2024.6.2/platform/common/toolchain/inc/sl_gcc_preinclude.h \
- ../autogen/sl_ot_init.h
-C:/Users/secerdan/SimplicityStudio/v5_workspace_12/MatterLightOverThreadSolution_dual_instance/config/sl_openthread_ble_cli_config.h:
-C:/Users/secerdan/SimplicityStudio/v5_workspace_12/MatterLightOverThreadSolution_dual_instance/config/sl_openthread_rtos_config.h:
-C:/Users/secerdan/SimplicityStudio/v5_workspace_12/MatterLightOverThreadSolution_dual_instance/simplicity_sdk_2024.6.2/platform/common/toolchain/inc/sl_gcc_preinclude.h:
-../autogen/sl_ot_init.h:
